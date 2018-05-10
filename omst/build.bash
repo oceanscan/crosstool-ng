ebb83c46a8b43100ce370ff0e91e5702e1d426c0
Ricardo Martins <rasm@oceanscan-mst.com>
2018-05-10 14:24:03 +0100

    Added preliminary build script and OMST board samples.

---
+++ omst/build.bash
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+VERSION='2018.05.00'
+BASE="$(readlink -f "$(dirname "$BASH_SOURCE")/..")"
+DST="$BASE/dist"
+TCHAINS='omst-rpi3'
+
+export PATH="$BASE/local/bin:$PATH"
+
+die()
+{
+    echo "ERROR: failed to $1"
+    exit 1
+}
+
+container_create()
+{
+    docker build -t crosstool-ng "$BASE/omst"
+}
+
+# Install crosstool-ng tools.
+xtool_bootstrap()
+{
+    if ! [ -d "$BASE/local" ]; then
+        ./bootstrap || die "bootstrap crosstool-ng"
+        ./configure --prefix="$BASE/local" || die "configure crosstool-ng"
+        make install || die "install crosstool-ng"
+    fi
+}
+
+build()
+{
+    mkdir -p "$BASE/src" "$BASE/dist" || die "create folders"
+
+    # Build toolchains.
+    for t in $TCHAINS; do
+        ct-ng "$t" || die "configure $t"
+        echo 'CT_ALLOW_BUILD_AS_ROOT_SURE=y' >> .config || die "override config"
+        echo "CT_LOCAL_TARBALLS_DIR=$DST" >> .config || die "override config"
+        echo "CT_PREFIX_DIR=$DST/"'${CT_TARGET}'"-${VERSION}" >> .config || die "override config"
+        ct-ng build.8 || die "build toolchain $t"
+    done
+
+    # Package toolchains.
+    cd $DST || die "change to dist folder"
+    for d in *omst*; do
+        if [ -d "$d" ]; then
+            name="$(basename "$d")"
+            tar -cvJf "$name".tar.xz "$d" || die "pack $d"
+        fi
+    done
+}
+
+# Change to crosstool-ng folder.
+cd "$BASE" || die "changed to base folder"
+
+if [ "$1" == "bootstrap" ]; then
+    container_create || die "create container"
+
+    docker run \
+           --read-only \
+           --tmpfs /run \
+           --tmpfs /tmp \
+           -v "$BASE:/xtool" \
+           -i \
+           -a stdin \
+           -a stdout \
+           -t "crosstool-ng" \
+           /bin/bash
+else
+    xtool_bootstrap
+    build
+fi
